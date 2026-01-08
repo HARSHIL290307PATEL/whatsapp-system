e6b022ddc15e4426e0724ed2753cb610c72c5d0e
Harshil <hasrhil2937patel@gmail.com>
2026-01-09 01:25:36 +0530

    Optimize Puppeteer args

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ function createSession(userId) {
                 '--disable-accelerated-2d-canvas',
                 '--no-first-run',
                 '--no-zygote',
-                '--single-process',
                 '--disable-gpu'
-            ]
+            ],
+            timeout: 60000
         }
     });
 
